6d83c5452401e8a813d4081dc5da6b8fbbd19441
Gurkirat9200 <gurkiratsinghjangla@gmail.com>
2026-07-07 10:43:59 +0530

    Add separate max subarray implementation

---
+++ MainSeparate.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MainSeparate {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                maxSum = Math.max(maxSum, currentSum);
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(maxSubArray(nums));
+
+        sc.close();
+    }
+}
+
